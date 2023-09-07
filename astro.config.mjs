b2938171f186b59748ebb275dc9d9744775ca3c6
Miro Ostafiński <ostafinskim@gmail.com>
2023-09-07 09:08:14 +0100

    feat: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
+import solidJs from '@astrojs/solid-js';
+import react from '@astrojs/react';
 
-import solidJs from "@astrojs/solid-js";
-import react from "@astrojs/react";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), react()]
+  site: '',
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true
+  },
+  integrations: [solidJs(), react()],
+  adapter: netlify()
 });
---
+++ src/content/_config.ts
@@ -0,0 +1,13 @@
+// configure content collection in Astro
+
+import { defineCollection, z } from 'astro:content';
+
+export const collections = {
+	blog: defineCollection({
+		schema: z.object({
+			title: z.string(),
+			date: z.date(),
+            description: z.string().max(200),
+		}),
+	}),
+};
